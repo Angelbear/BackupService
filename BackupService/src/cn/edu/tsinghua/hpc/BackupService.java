96adf464e274c01b9995ce2214a71598bb9a1bb1
Yangyang <yangyang@aurorafeint.com>
2011-07-27 11:30:08 +0800

    1. Fix the cloudstorage function 2. Add the backupservice logic

--- BackupService/src/cn/edu/tsinghua/hpc/BackupService.java
+++ BackupService/src/cn/edu/tsinghua/hpc/BackupService.java
@@ -1,7 +1,8 @@
 
 package cn.edu.tsinghua.hpc;
 
-import com.ccit.phone.CCITSC;
+import cn.edu.tsinghua.hpc.ca.CAUtils;
+
 import com.ccit.phone.LoginView;
 
 import org.bouncycastle.util.encoders.Base64;
@@ -13,13 +14,25 @@ import android.content.Intent;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.IBinder;
+import android.os.Parcel;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.telephony.TelephonyManager;
-import cn.edu.tsinghua.hpc.ca.*;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class BackupService extends Service {
+
+    private String uid;
+
+    private String token;
+
+    ConnectionListener listener;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -32,76 +45,104 @@ public class BackupService extends Service {
     private void backupSettings() {
         try {
             JSONObject json = new JSONObject();
-            json.put(Settings.System.RINGTONE, Settings.System.getString(getContentResolver(),
-                    Settings.System.RINGTONE));
-            json.put(Settings.System.AUTO_TIME, Settings.System.getInt(getContentResolver(),
-                    Settings.System.AUTO_TIME));
+            json.put(Settings.System.RINGTONE,
+                    Settings.System.getString(getContentResolver(), Settings.System.RINGTONE));
+            json.put(Settings.System.AUTO_TIME,
+                    Settings.System.getInt(getContentResolver(), Settings.System.AUTO_TIME));
+            CloudStorage.putFile("settings", uid, token, new ByteArrayInputStream(json.toString()
+                    .getBytes()), json.toString().getBytes().length);
             BitmapDrawable wallpaper = (BitmapDrawable) getWallpaper();
-            FileOutputStream fos = null;
-            wallpaper.getBitmap().compress(CompressFormat.PNG, 100, fos);
-
+            Parcel p = Parcel.obtain();
+            wallpaper.getBitmap().writeToParcel(p, 0);
+            CloudStorage.putFile("wallpaper", uid, token,
+                    new ByteArrayInputStream(p.createByteArray()), p.dataSize());
         } catch (SettingNotFoundException e) {
         } catch (JSONException e) {
         }
     }
 
-    private void restoreSettings(JSONObject json) {
+    private static String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
         try {
-            Settings.System.putString(getContentResolver(), Settings.System.RINGTONE, json
-                    .getString(Settings.System.RINGTONE));
-            Settings.System.putInt(getContentResolver(), Settings.System.AUTO_TIME, json
-                    .getInt(Settings.System.RINGTONE));
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+            }
+        }
+        return sb.toString();
+    }
+
+    private void restoreSettings() {
+        try {
+            JSONObject json = new JSONObject(convertStreamToString(CloudStorage.downloadFile(
+                    "settings", uid, token)));
+            Settings.System.putString(getContentResolver(), Settings.System.RINGTONE,
+                    json.getString(Settings.System.RINGTONE));
+            Settings.System.putInt(getContentResolver(), Settings.System.AUTO_TIME,
+                    json.getInt(Settings.System.RINGTONE));
+            BackupService.this.setWallpaper(CloudStorage.downloadFile("wallpaper", uid, token));
             preferences_restored = true;
         } catch (JSONException e) {
+        } catch (IOException e) {
         }
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        try {
-            CAUtils mUtils = CAUtils.getInstance(this);
-
-            LoginView loginViewForCPK = mUtils.requestLoginForCPK();
-            String uid = loginViewForCPK.getUID();
-            String signature = new String(Base64.encode(loginViewForCPK.getSignature()));//
-            String result = mUtils.verifySignForCPK(this, CAUtils.ip, CAUtils.port, uid, signature);
-            if (result != null && result.equals("0")) {
-
-            }
-
-
-        } catch (Exception e1) {
-        }
-
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                TelephonyManager manager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-                // Phase 1: retrieve the imsi info
-                while (manager.getSubscriberId() == null) {
-                    try {
-                        Thread.sleep(3000);
-                    } catch (InterruptedException e) {
-                    }
-                }
-                while (true) {
-                    if (!preferences_restored) {
-
-                    } else {
-
-                    }
-                }
-
-            }
-        }).start();
+        listener = new ConnectionListener(BackupService.this);
     }
 
     @Override
     public void onStart(Intent intent, int startId) {
+        while (true) {
+            if (listener.isConnected()) {
+                try {
+                    CAUtils mUtils = CAUtils.getInstance(this);
+                    LoginView loginViewForCPK = mUtils.requestLoginForCPK();
+                    uid = loginViewForCPK.getUID();
+                    token = new String(Base64.encode(loginViewForCPK.getSignature()));
+                    String result = mUtils.verifySignForCPK(this, CAUtils.ip, CAUtils.port, uid,
+                            token);
+                    if (result != null && result.equals("0")) {
+                    }
+                } catch (Exception e) {
+                    return;
+                }
 
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        TelephonyManager manager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+                        // Phase 1: retrieve the imsi info
+                        while (manager.getSubscriberId() == null) {
+                            try {
+                                Thread.sleep(3000);
+                            } catch (InterruptedException e) {
+                            }
+                        }
+                        while (true) {
+                            if (!preferences_restored) {
+                                restoreSettings();
+                            } else {
+                                backupSettings();
+                            }
+                        }
+
+                    }
+                }).start();
+                break;
+            }
+        }
     }
 
     @Override
--- BackupService/src/cn/edu/tsinghua/hpc/CloudStorage.java
+++ BackupService/src/cn/edu/tsinghua/hpc/CloudStorage.java
@@ -1,3 +1,4 @@
+
 package cn.edu.tsinghua.hpc;
 
 import java.io.IOException;
@@ -15,63 +16,59 @@ import org.apache.http.entity.InputStreamEntity;
 import android.net.http.AndroidHttpClient;
 
 public class CloudStorage {
-	private static final String CLOUD_STORAGE_URL = "http://210.75.5.232:8085/";
-	private static final String MYPREFERENCES_URL_TEP = "http://210.75.5.232:8085/%s/myPreferences/%s";
+    private static final String MYPREFERENCES_URL_TEP = "http://210.75.5.232:8085/%s/myPreferences/%s";
 
-	private boolean putFile(String file, String uid, String token,
-			InputStream is, long length) {
-		String url = String.format(MYPREFERENCES_URL_TEP, uid, file);
-		String authInfo = String.format("CPK %s:%s", uid, token);
-		HttpPost p = new HttpPost(url);
-		p.setEntity(new InputStreamEntity(is, length));
-		p.setHeader("Authorization", authInfo);
-		try {
-			HttpResponse r = AndroidHttpClient.newInstance("Android")
-					.execute(p);
-			if (r.getStatusLine().getStatusCode() == 200) {
-				return true;
-			}
-		} catch (IOException e) {
+    public static boolean putFile(String file, String uid, String token, InputStream is, long length) {
+        String url = String.format(MYPREFERENCES_URL_TEP, uid, file);
+        String authInfo = String.format("CPK %s:%s", uid, token);
+        HttpPost p = new HttpPost(url);
+        p.setEntity(new InputStreamEntity(is, length));
+        p.setHeader("Authorization", authInfo);
+        try {
+            HttpResponse r = AndroidHttpClient.newInstance("Android").execute(p);
+            if (r.getStatusLine().getStatusCode() == HttpURLConnection.HTTP_OK) {
+                return true;
+            }
+        } catch (IOException e) {
 
-		}
-		return false;
-	}
+        }
+        return false;
+    }
 
-	private boolean deleteFile(String file, String uid, String token,
-			InputStream is, long length) {
+    public static boolean deleteFile(String file, String uid, String token, InputStream is,
+            long length) {
 
-		String authInfo = String.format("CPK %s:%s", uid, token);
-		URL url;
-		try {
-			url = new URL(String.format(MYPREFERENCES_URL_TEP, uid, file));
-			URLConnection connection = url.openConnection();
-			HttpURLConnection httpConnection = (HttpURLConnection) connection;
-			httpConnection.setRequestMethod("DELETE");
-			int responseCode = httpConnection.getResponseCode();
+        String authInfo = String.format("CPK %s:%s", uid, token);
+        URL url;
+        try {
+            url = new URL(String.format(MYPREFERENCES_URL_TEP, uid, file));
+            URLConnection connection = url.openConnection();
+            HttpURLConnection httpConnection = (HttpURLConnection) connection;
+            httpConnection.setRequestMethod("DELETE");
+            connection.setRequestProperty("Authorization", authInfo);
+            int responseCode = httpConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                return true;
+            }
+        } catch (MalformedURLException e) {
+        } catch (IOException e) {
+        }
 
-			if (responseCode == HttpURLConnection.HTTP_OK) {
-				return true;
-			}
-		} catch (MalformedURLException e) {
-		} catch (IOException e) {
-		}
+        return false;
+    }
 
-		return false;
-	}
+    public static InputStream downloadFile(String file, String uid, String token) {
+        String url = String.format(MYPREFERENCES_URL_TEP, uid, file);
+        String authInfo = String.format("CPK %s:%s", uid, token);
+        HttpGet g = new HttpGet(url);
+        g.setHeader("Authorization", authInfo);
+        try {
+            HttpResponse r = AndroidHttpClient.newInstance("Android").execute(g);
+            return r.getEntity().getContent();
+        } catch (IOException e) {
 
-	private InputStream downloadFile(String file, String uid, String token) {
-		String url = String.format(MYPREFERENCES_URL_TEP, uid, file);
-		String authInfo = String.format("CPK %s:%s", uid, token);
-		HttpGet g = new HttpGet(url);
-		g.setHeader("Authorization", authInfo);
-		try {
-			HttpResponse r = AndroidHttpClient.newInstance("Android")
-					.execute(g);
-			return r.getEntity().getContent();
-		} catch (IOException e) {
+        }
 
-		}
-
-		return null;
-	}
+        return null;
+    }
 }
