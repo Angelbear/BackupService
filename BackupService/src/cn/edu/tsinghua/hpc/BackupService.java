3d3021466e215271cabe6db273f2698a73383d16
Yangyang Zhao <yangyang.zhao.thu@gmail.com>
2011-08-09 12:51:16 +0800

    1.Change the httpclient to multithread-safe mode 2.Change the backup policy: delete before upload

--- BackupService/src/cn/edu/tsinghua/hpc/BackupService.java
+++ BackupService/src/cn/edu/tsinghua/hpc/BackupService.java
@@ -1,4 +1,3 @@
-
 package cn.edu.tsinghua.hpc;
 
 import java.io.BufferedReader;
@@ -32,179 +31,194 @@ import com.ccit.phone.LoginView;
 
 public class BackupService extends Service {
 
-    private String uid;
+	private String uid;
 
-    private String token;
+	private String token;
 
-    ConnectionListener listener;
+	ConnectionListener listener;
 
-    private static boolean firstStart = true;
+	private static boolean firstStart = true;
 
-    private static final String TAG = "BackupService";
+	private static final String TAG = "BackupService";
 
-    public interface IMyService {
-        public void backup();
-    }
+	public interface IMyService {
+		public void backup();
+	}
 
-    private MyServiceBinder myServiceBinder = new MyServiceBinder();
+	private MyServiceBinder myServiceBinder = new MyServiceBinder();
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return myServiceBinder;
-    }
+	@Override
+	public IBinder onBind(Intent intent) {
+		return myServiceBinder;
+	}
 
-    public class MyServiceBinder extends Binder implements IMyService {
-        public void backup() {
-            if (!preferences_restored) {
-                restoreSettings();
-            } else {
-                backupSettings();
-            }
-        }
-    }
+	public class MyServiceBinder extends Binder implements IMyService {
+		public void backup() {
+			if (!preferences_restored) {
+				restoreSettings();
+			} else {
+				backupSettings();
+			}
+		}
+	}
 
-    private static boolean preferences_restored = false;
+	private static boolean preferences_restored = false;
 
-    private void backupSettings() {
-        Log.d(TAG, "begin backup");
-        try {
-            JSONObject json = new JSONObject();
-            json.put(Settings.System.RINGTONE,
-                    Settings.System.getString(getContentResolver(), Settings.System.RINGTONE));
-            json.put(Settings.System.AUTO_TIME,
-                    Settings.System.getInt(getContentResolver(), Settings.System.AUTO_TIME));
-            json.put(Settings.System.NOTIFICATION_SOUND, Settings.System.getString(
-                    getContentResolver(), Settings.System.NOTIFICATION_SOUND));
-            json.put("TimeZone", TimeZone.getDefault().getDisplayName());
+	private void backupSettings() {
+		Log.d(TAG, "begin backup");
+		try {
+			JSONObject json = new JSONObject();
+			json.put(Settings.System.RINGTONE, Settings.System.getString(
+					getContentResolver(), Settings.System.RINGTONE));
+			json.put(Settings.System.AUTO_TIME, Settings.System.getInt(
+					getContentResolver(), Settings.System.AUTO_TIME));
+			json.put(Settings.System.NOTIFICATION_SOUND, Settings.System
+					.getString(getContentResolver(),
+							Settings.System.NOTIFICATION_SOUND));
+			json.put("TimeZone", TimeZone.getDefault().getID());
 
-            Log.d(TAG, "Settings are " + json.toString());
-            CloudStorage.getInstance().putFile("settings", uid, token,
-                    new ByteArrayInputStream(json.toString().getBytes()),
-                    json.toString().getBytes().length);
-            BitmapDrawable wallpaper = (BitmapDrawable) getWallpaper();
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            wallpaper.getBitmap().compress(Bitmap.CompressFormat.PNG, 100, baos);
-            CloudStorage.getInstance().putFile("wallpaper", uid, token,
-                    new ByteArrayInputStream(baos.toByteArray()), baos.size());
-            Log.d(TAG, "finsh backup");
-        } catch (SettingNotFoundException e) {
-        } catch (JSONException e) {
-        }
-    }
+			if (CloudStorage.getInstance().deleteFile("settings", uid, token)) {
+				Log.d(TAG, "delete settings success");
+			}
+			Log.d(TAG, "Settings are " + json.toString());
+			CloudStorage.getInstance().putFile("settings", uid, token,
+					new ByteArrayInputStream(json.toString().getBytes()),
+					json.toString().getBytes().length);
+			BitmapDrawable wallpaper = (BitmapDrawable) getWallpaper();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			wallpaper.getBitmap()
+					.compress(Bitmap.CompressFormat.PNG, 100, baos);
+			if (CloudStorage.getInstance().deleteFile("wallpaper", uid, token)) {
+				Log.d(TAG, "delete wallpaper success");
+			}
+			CloudStorage.getInstance().putFile("wallpaper", uid, token,
+					new ByteArrayInputStream(baos.toByteArray()), baos.size());
+			Log.d(TAG, "finsh backup");
+		} catch (SettingNotFoundException e) {
+		} catch (JSONException e) {
+		}
+	}
 
-    private static String convertStreamToString(InputStream is) throws NullPointerException {
-        if (is == null)
-            throw new NullPointerException();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
+	private static String convertStreamToString(InputStream is)
+			throws NullPointerException {
+		if (is == null)
+			throw new NullPointerException();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		StringBuilder sb = new StringBuilder();
 
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-            }
-        }
-        return sb.toString();
-    }
+		String line = null;
+		try {
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+		} catch (IOException e) {
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+			}
+		}
+		return sb.toString();
+	}
 
-    private void restoreSettings() {
-        Log.d(TAG, "begin restore");
-        try {
-            startActivity(new Intent(this, DialogActivity.class)
-                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-            String content = convertStreamToString(CloudStorage.getInstance().downloadFile(
-                    "settings", uid, token));
-            Log.d(TAG, "Settings are " + content);
-            JSONObject json = new JSONObject(content);
-            Log.d(TAG, "Settings are " + json.toString());
-            Settings.System.putString(getContentResolver(), Settings.System.RINGTONE,
-                    json.getString(Settings.System.RINGTONE));
-            Settings.System.putInt(getContentResolver(), Settings.System.AUTO_TIME,
-                    json.getInt(Settings.System.AUTO_TIME));
-            Settings.System.putString(getContentResolver(), Settings.System.NOTIFICATION_SOUND,
-                    json.getString(Settings.System.NOTIFICATION_SOUND));
-            AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-            alarm.setTimeZone(json.getString("TimeZone"));
-            BackupService.this.setWallpaper(CloudStorage.getInstance().downloadFile("wallpaper",
-                    uid, token));
-            preferences_restored = true;
-            Log.d(TAG, "finish restore");
-        } catch (JSONException e) {
-            preferences_restored = true;
-        } catch (IOException e) {
-        } catch (NullPointerException e) {
+	private void restoreSettings() {
+		Log.d(TAG, "begin restore");
+		try {
+			startActivity(new Intent(this, DialogActivity.class)
+					.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+			String content = convertStreamToString(CloudStorage.getInstance()
+					.downloadFile("settings", uid, token));
+			Log.d(TAG, "Settings are " + content);
+			JSONObject json = new JSONObject(content);
+			Log.d(TAG, "Settings are " + json.toString());
+			Settings.System.putString(getContentResolver(),
+					Settings.System.RINGTONE,
+					json.getString(Settings.System.RINGTONE));
+			Settings.System.putInt(getContentResolver(),
+					Settings.System.AUTO_TIME,
+					json.getInt(Settings.System.AUTO_TIME));
+			Settings.System.putString(getContentResolver(),
+					Settings.System.NOTIFICATION_SOUND,
+					json.getString(Settings.System.NOTIFICATION_SOUND));
+			AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+			alarm.setTimeZone(json.getString("TimeZone"));
+			BackupService.this.setWallpaper(CloudStorage.getInstance()
+					.downloadFile("wallpaper", uid, token));
+			preferences_restored = true;
+			Log.d(TAG, "finish restore");
+		} catch (JSONException e) {
+			preferences_restored = true;
+		} catch (IOException e) {
+		} catch (NullPointerException e) {
 
-        }
-        sendBroadcast(new Intent("cn.edu.tsinghua.hpc.restore"));
-    }
+		}
+		sendBroadcast(new Intent("cn.edu.tsinghua.hpc.restore"));
+	}
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        listener = new ConnectionListener(BackupService.this);
-    }
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		listener = new ConnectionListener(BackupService.this);
+	}
 
-    @Override
-    public void onStart(Intent intent, int startId) {
-        if (firstStart) {
-            new Thread(new Runnable() {
+	@Override
+	public void onStart(Intent intent, int startId) {
+		if (firstStart) {
+			new Thread(new Runnable() {
 
-                @Override
-                public void run() {
-                    while (true) {
-                        if (listener.isConnected()) {
-                            try {
-                                CCITSC mCCIT = new CCITSC(BackupService.this, CAUtils.ip,
-                                        CAUtils.port);
-                                mCCIT.loginInit(false);
-                                LoginView lv = mCCIT.requestLogin(false);
-                                uid = lv.getUID();
-                                token = new String(Base64.encode(lv.getSignature()));
-                                Log.d(TAG, "uid is " + uid + " token is " + token);
-                            } catch (Exception e) {
-                                continue;
-                            }
+				@Override
+				public void run() {
+					while (true) {
+						if (listener.isConnected()) {
+							try {
+								CCITSC mCCIT = new CCITSC(BackupService.this,
+										CAUtils.ip, CAUtils.port);
+								mCCIT.loginInit(false);
+								LoginView lv = mCCIT.requestLogin(false);
+								uid = lv.getUID();
+								token = new String(Base64.encode(lv
+										.getSignature()));
+								Log.d(TAG, "uid is " + uid + " token is "
+										+ token);
+							} catch (Exception e) {
+								continue;
+							}
 
-                            break;
-                        }
-                    }
-                    Intent alarmIntent = new Intent(BackupService.this,
-                            RepeatingAlarmReceiver.class);
-                    PendingIntent pendingIntent = PendingIntent.getBroadcast(BackupService.this, 0,
-                            alarmIntent, 0);
+							break;
+						}
+					}
+					Intent alarmIntent = new Intent(BackupService.this,
+							RepeatingAlarmReceiver.class);
+					PendingIntent pendingIntent = PendingIntent.getBroadcast(
+							BackupService.this, 0, alarmIntent, 0);
 
-                    Log.d(TAG, "startup alarm");
-                    AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-                    alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
-                            + (5 * 1000), 60 * 1000, pendingIntent);
-                    firstStart = false;
-                }
-            }).start();
-        } else {
-            new Thread(new Runnable() {
+					Log.d(TAG, "startup alarm");
+					AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+					alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
+							System.currentTimeMillis() + (5 * 1000), 60 * 1000,
+							pendingIntent);
+					firstStart = false;
+				}
+			}).start();
+		} else {
+			new Thread(new Runnable() {
 
-                @Override
-                public void run() {
-                    if (!preferences_restored) {
-                        restoreSettings();
-                    } else {
-                        backupSettings();
-                    }
-                }
-            }).start();
+				@Override
+				public void run() {
+					if (!preferences_restored) {
+						restoreSettings();
+					} else {
+						backupSettings();
+					}
+				}
+			}).start();
 
-        }
-    }
+		}
+	}
 
-    @Override
-    public void onDestroy() {
-        
-    }
+	@Override
+	public void onDestroy() {
+
+	}
 
 }
--- BackupService/src/cn/edu/tsinghua/hpc/CloudStorage.java
+++ BackupService/src/cn/edu/tsinghua/hpc/CloudStorage.java
@@ -10,38 +10,46 @@ import java.net.URLConnection;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
-
-import android.util.Log;
 
 public class CloudStorage {
 	private static final String MYPREFERENCES_URL_TEP = "http://210.75.5.232:8085/%s/myPreferences/%s";
 
 	private static DefaultHttpClient client = null;
-	
+
 	private static CloudStorage mInstance = null;
-	
+
 	private static final String TAG = "CloudStorage";
-	
+
 	private CloudStorage() {
-		if(client == null) {	
+		if (client == null) {
 			HttpParams httpParams = new BasicHttpParams();
-		    HttpConnectionParams.setConnectionTimeout(httpParams, 300000);
-		    HttpConnectionParams.setSoTimeout(httpParams, 300000);
-		    client = new DefaultHttpClient(httpParams);
+			HttpConnectionParams.setConnectionTimeout(httpParams, 300000);
+			HttpConnectionParams.setSoTimeout(httpParams, 300000);
+			SchemeRegistry schemeRegistry = new SchemeRegistry();
+			schemeRegistry.register(new Scheme("http", PlainSocketFactory
+					.getSocketFactory(), 8085));
+			ClientConnectionManager cm = new ThreadSafeClientConnManager(
+					httpParams, schemeRegistry);
+			client = new DefaultHttpClient(cm, httpParams);
 		}
 	}
-	
+
 	public static CloudStorage getInstance() {
-		if(mInstance == null) 
+		if (mInstance == null)
 			mInstance = new CloudStorage();
 		return mInstance;
 	}
-	
+
 	public boolean putFile(String file, String uid, String token,
 			InputStream is, long length) {
 		String url = String.format(MYPREFERENCES_URL_TEP, uid, file);
@@ -59,8 +67,7 @@ public class CloudStorage {
 		return false;
 	}
 
-	public boolean deleteFile(String file, String uid, String token,
-			InputStream is, long length) {
+	public boolean deleteFile(String file, String uid, String token) {
 
 		String authInfo = String.format("ASP %s:%s", uid, token);
 		URL url;
