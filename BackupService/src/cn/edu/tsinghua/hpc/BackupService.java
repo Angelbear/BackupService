cf6e15793dc3a99e6d90f988e39517ee6846776f
Yangyang <yangyang@aurorafeint.com>
2011-08-16 10:21:38 +0800

    service update

--- BackupService/src/cn/edu/tsinghua/hpc/BackupService.java
+++ BackupService/src/cn/edu/tsinghua/hpc/BackupService.java
@@ -1,12 +1,10 @@
+
 package cn.edu.tsinghua.hpc;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.TimeZone;
+import cn.edu.tsinghua.hpc.ca.CAUtils;
+
+import com.ccit.phone.CCITSC;
+import com.ccit.phone.LoginView;
 
 import org.bouncycastle.util.encoders.Base64;
 import org.json.JSONException;
@@ -21,204 +19,237 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
-import cn.edu.tsinghua.hpc.ca.CAUtils;
 
-import com.ccit.phone.CCITSC;
-import com.ccit.phone.LoginView;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.TimeZone;
 
 public class BackupService extends Service {
 
-	private String uid;
+    private String uid;
 
-	private String token;
+    private String token;
 
-	ConnectionListener listener;
+    ConnectionListener listener;
 
-	private static boolean firstStart = true;
+    private static boolean firstStart = true;
 
-	private static final String TAG = "BackupService";
+    private static final String TAG = "BackupService";
 
-	public interface IMyService {
-		public void backup();
-	}
+    public interface IMyService {
+        public void backup();
+    }
 
-	private MyServiceBinder myServiceBinder = new MyServiceBinder();
+    private MyServiceBinder myServiceBinder = new MyServiceBinder();
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		return myServiceBinder;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        return myServiceBinder;
+    }
 
-	public class MyServiceBinder extends Binder implements IMyService {
-		public void backup() {
-			if (!preferences_restored) {
-				restoreSettings();
-			} else {
-				backupSettings();
-			}
-		}
-	}
+    public class MyServiceBinder extends Binder implements IMyService {
+        public void backup() {
+            if (!preferences_restored) {
+                restoreSettings();
+            } else {
+                backupSettings();
+            }
+        }
+    }
 
-	private static boolean preferences_restored = false;
+    private static boolean preferences_restored = false;
 
-	private void backupSettings() {
-		Log.d(TAG, "begin backup");
-		try {
-			JSONObject json = new JSONObject();
-			json.put(Settings.System.RINGTONE, Settings.System.getString(
-					getContentResolver(), Settings.System.RINGTONE));
-			json.put(Settings.System.AUTO_TIME, Settings.System.getInt(
-					getContentResolver(), Settings.System.AUTO_TIME));
-			json.put(Settings.System.NOTIFICATION_SOUND, Settings.System
-					.getString(getContentResolver(),
-							Settings.System.NOTIFICATION_SOUND));
-			json.put("TimeZone", TimeZone.getDefault().getID());
+    private void backupSettings() {
+        Log.d(TAG, "begin backup");
+        try {
+            JSONObject json = new JSONObject();
+            json.put(Settings.System.RINGTONE,
+                    Settings.System.getString(getContentResolver(), Settings.System.RINGTONE));
+            json.put(Settings.System.AUTO_TIME,
+                    Settings.System.getInt(getContentResolver(), Settings.System.AUTO_TIME));
+            json.put(Settings.System.NOTIFICATION_SOUND, Settings.System.getString(
+                    getContentResolver(), Settings.System.NOTIFICATION_SOUND));
+            json.put("TimeZone", TimeZone.getDefault().getID());
 
-			if (CloudStorage.getInstance().deleteFile("settings", uid, token)) {
-				Log.d(TAG, "delete settings success");
-			}
-			Log.d(TAG, "Settings are " + json.toString());
-			CloudStorage.getInstance().putFile("settings", uid, token,
-					new ByteArrayInputStream(json.toString().getBytes()),
-					json.toString().getBytes().length);
-			BitmapDrawable wallpaper = (BitmapDrawable) getWallpaper();
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			wallpaper.getBitmap()
-					.compress(Bitmap.CompressFormat.PNG, 100, baos);
-			if (CloudStorage.getInstance().deleteFile("wallpaper", uid, token)) {
-				Log.d(TAG, "delete wallpaper success");
-			}
-			CloudStorage.getInstance().putFile("wallpaper", uid, token,
-					new ByteArrayInputStream(baos.toByteArray()), baos.size());
-			Log.d(TAG, "finsh backup");
-		} catch (SettingNotFoundException e) {
-		} catch (JSONException e) {
-		}
-	}
+            if (CloudStorage.getInstance().deleteFile("settings", uid, token)) {
+                Log.d(TAG, "delete settings success");
+            }
+            Log.d(TAG, "Settings are " + json.toString());
+            if (CloudStorage.getInstance().putFile("settings", uid, token,
+                    new ByteArrayInputStream(json.toString().getBytes()),
+                    json.toString().getBytes().length)) {
+                Log.d(TAG, "upload settings success");
+            }
+            BitmapDrawable wallpaper = (BitmapDrawable) getWallpaper();
+            FileOutputStream fos;
+            try {
+                fos = this.openFileOutput("wallpaper", MODE_PRIVATE);
+                wallpaper.getBitmap().compress(Bitmap.CompressFormat.PNG, 100, fos);
+                fos.flush();
+                fos.close();
+            } catch (FileNotFoundException e) {
+            } catch (IOException e) {
+            }
+            
+            //if (CloudStorage.getInstance().deleteFile("wallpaper", uid, token)) {
+            //    Log.d(TAG, "delete wallpaper success");
+            //}
 
-	private static String convertStreamToString(InputStream is)
-			throws NullPointerException {
-		if (is == null)
-			throw new NullPointerException();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-		StringBuilder sb = new StringBuilder();
+            if (CloudStorage.getInstance()
+                    .putFile(this.getFileStreamPath("wallpaper").getAbsolutePath(), "wallpaper",
+                            uid, token/*
+                                       * , this.openFileInput("wallpaper") ,
+                                       * this.getFileStreamPath(
+                                       * "wallpaper").length()
+                                       */)) {
+                Log.d(TAG, "upload wallpaper success");
+            }
+            this.getFileStreamPath("wallpaper").delete();
+            Log.d(TAG, "finsh backup");
+        } catch (SettingNotFoundException e) {
+        } catch (JSONException e) {
+        }
+    }
 
-		String line = null;
-		try {
-			while ((line = reader.readLine()) != null) {
-				sb.append(line + "\n");
-			}
-		} catch (IOException e) {
-		} finally {
-			try {
-				is.close();
-			} catch (IOException e) {
-			}
-		}
-		return sb.toString();
-	}
+    private static String convertStreamToString(InputStream is) throws NullPointerException {
+        if (is == null)
+            throw new NullPointerException();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
 
-	private void restoreSettings() {
-		Log.d(TAG, "begin restore");
-		try {
-			startActivity(new Intent(this, DialogActivity.class)
-					.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-			String content = convertStreamToString(CloudStorage.getInstance()
-					.downloadFile("settings", uid, token));
-			Log.d(TAG, "Settings are " + content);
-			JSONObject json = new JSONObject(content);
-			Log.d(TAG, "Settings are " + json.toString());
-			Settings.System.putString(getContentResolver(),
-					Settings.System.RINGTONE,
-					json.getString(Settings.System.RINGTONE));
-			Settings.System.putInt(getContentResolver(),
-					Settings.System.AUTO_TIME,
-					json.getInt(Settings.System.AUTO_TIME));
-			Settings.System.putString(getContentResolver(),
-					Settings.System.NOTIFICATION_SOUND,
-					json.getString(Settings.System.NOTIFICATION_SOUND));
-			AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-			alarm.setTimeZone(json.getString("TimeZone"));
-			BackupService.this.setWallpaper(CloudStorage.getInstance()
-					.downloadFile("wallpaper", uid, token));
-			preferences_restored = true;
-			Log.d(TAG, "finish restore");
-		} catch (JSONException e) {
-			preferences_restored = true;
-		} catch (IOException e) {
-		} catch (NullPointerException e) {
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+            }
+        }
+        return sb.toString();
+    }
 
-		}
-		sendBroadcast(new Intent("cn.edu.tsinghua.hpc.restore"));
-	}
+    private void restoreSettings() {
+        Log.d(TAG, "begin restore");
+        try {
+            //startActivity(new Intent(this, DialogActivity.class)
+            //        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+            String content = convertStreamToString(CloudStorage.getInstance().downloadFile(
+                    "settings", uid, token));
+            Log.d(TAG, "Settings are " + content);
+            JSONObject json = new JSONObject(content);
+            Log.d(TAG, "Settings are " + json.toString());
+            Settings.System.putString(getContentResolver(), Settings.System.RINGTONE,
+                    json.getString(Settings.System.RINGTONE));
+            Settings.System.putInt(getContentResolver(), Settings.System.AUTO_TIME,
+                    json.getInt(Settings.System.AUTO_TIME));
+            Settings.System.putString(getContentResolver(), Settings.System.NOTIFICATION_SOUND,
+                    json.getString(Settings.System.NOTIFICATION_SOUND));
+            AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            alarm.setTimeZone(json.getString("TimeZone"));
+            BackupService.this.setWallpaper(CloudStorage.getInstance().downloadFile("wallpaper",
+                    uid, token));
+            preferences_restored = true;
+            Log.d(TAG, "finish restore");
+        } catch (JSONException e) {
+            preferences_restored = true;
+        } catch (IOException e) {
+        } catch (NullPointerException e) {
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		listener = new ConnectionListener(BackupService.this);
-	}
+        }
+        //sendBroadcast(new Intent("cn.edu.tsinghua.hpc.restore"));
+    }
 
-	@Override
-	public void onStart(Intent intent, int startId) {
-		if (firstStart) {
-			new Thread(new Runnable() {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        listener = new ConnectionListener(BackupService.this);
+    }
 
-				@Override
-				public void run() {
-					while (true) {
-						if (listener.isConnected()) {
-							try {
-								CCITSC mCCIT = new CCITSC(BackupService.this,
-										CAUtils.ip, CAUtils.port);
-								mCCIT.loginInit(false);
-								LoginView lv = mCCIT.requestLogin(false);
-								uid = lv.getUID();
-								token = new String(Base64.encode(lv
-										.getSignature()));
-								Log.d(TAG, "uid is " + uid + " token is "
-										+ token);
-							} catch (Exception e) {
-								continue;
-							}
+    @Override
+    public void onStart(Intent intent, int startId) {
+        if (firstStart) {
+            new Thread(new Runnable() {
 
-							break;
-						}
-					}
-					Intent alarmIntent = new Intent(BackupService.this,
-							RepeatingAlarmReceiver.class);
-					PendingIntent pendingIntent = PendingIntent.getBroadcast(
-							BackupService.this, 0, alarmIntent, 0);
+                @Override
+                public void run() {
+                    PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+                    PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                            getClass().getName());
+                    wl.acquire();
+                    while (true) {
+                        if (listener.isConnected()) {
+                            try {
+                                CCITSC mCCIT = new CCITSC(BackupService.this, CAUtils.ip,
+                                        CAUtils.port);
+                                mCCIT.loginInit(false);
+                                LoginView lv = mCCIT.requestLogin(false);
+                                uid = lv.getUID();
+                                token = new String(Base64.encode(lv.getSignature()));
+                                Log.d(TAG, "uid is " + uid + " token is " + token);
+                            } catch (Exception e) {
+                                continue;
+                            }
 
-					Log.d(TAG, "startup alarm");
-					AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-					alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
-							System.currentTimeMillis() + (5 * 1000), 60 * 1000,
-							pendingIntent);
-					firstStart = false;
-				}
-			}).start();
-		} else {
-			new Thread(new Runnable() {
+                            break;
+                        }
+                    }
+                    Intent alarmIntent = new Intent(BackupService.this,
+                            RepeatingAlarmReceiver.class);
+                    PendingIntent pendingIntent = PendingIntent.getBroadcast(BackupService.this, 0,
+                            alarmIntent, 0);
 
-				@Override
-				public void run() {
-					if (!preferences_restored) {
-						restoreSettings();
-					} else {
-						backupSettings();
-					}
-				}
-			}).start();
+                    Log.d(TAG, "startup alarm");
+                    AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                    alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
+                            + (5 * 1000), pendingIntent);
+                    firstStart = false;
+                    wl.release();
+                }
+            }).start();
+        } else {
+            new Thread(new Runnable() {
 
-		}
-	}
+                @Override
+                public void run() {
+                    PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+                    PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                            getClass().getName());
+                    wl.acquire();
+                    if (!preferences_restored) {
+                        restoreSettings();
+                    } else {
+                        backupSettings();
+                    }
+                    Intent alarmIntent = new Intent(BackupService.this,
+                            RepeatingAlarmReceiver.class);
+                    PendingIntent pendingIntent = PendingIntent.getBroadcast(BackupService.this, 0,
+                            alarmIntent, 0);
+                    AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                    alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
+                            + (60 * 1000), pendingIntent);
+                    wl.release();
+                }
+            }).start();
 
-	@Override
-	public void onDestroy() {
+        }
+    }
 
-	}
+    @Override
+    public void onDestroy() {
+
+    }
 
 }
--- BackupService/src/cn/edu/tsinghua/hpc/CloudStorage.java
+++ BackupService/src/cn/edu/tsinghua/hpc/CloudStorage.java
@@ -1,13 +1,9 @@
+
 package cn.edu.tsinghua.hpc;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.conn.ClientConnectionManager;
@@ -21,84 +17,178 @@ import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class CloudStorage {
-	private static final String MYPREFERENCES_URL_TEP = "http://210.75.5.232:8085/%s/myPreferences/%s";
+    private static final String MYPREFERENCES_URL_TEP = "http://210.75.5.232:8085/%s/myPreferences/%s";
 
-	private static DefaultHttpClient client = null;
+    private static DefaultHttpClient client = null;
 
-	private static CloudStorage mInstance = null;
+    private static CloudStorage mInstance = null;
 
-	private static final String TAG = "CloudStorage";
+    private static final String TAG = "BackupService";
 
-	private CloudStorage() {
-		if (client == null) {
-			HttpParams httpParams = new BasicHttpParams();
-			HttpConnectionParams.setConnectionTimeout(httpParams, 300000);
-			HttpConnectionParams.setSoTimeout(httpParams, 300000);
-			SchemeRegistry schemeRegistry = new SchemeRegistry();
-			schemeRegistry.register(new Scheme("http", PlainSocketFactory
-					.getSocketFactory(), 8085));
-			ClientConnectionManager cm = new ThreadSafeClientConnManager(
-					httpParams, schemeRegistry);
-			client = new DefaultHttpClient(cm, httpParams);
-		}
-	}
+    private static final long BLOCK_SIZE = 4096 * 10;
 
-	public static CloudStorage getInstance() {
-		if (mInstance == null)
-			mInstance = new CloudStorage();
-		return mInstance;
-	}
+    private CloudStorage() {
+        if (client == null) {
+            HttpParams httpParams = new BasicHttpParams();
+            HttpConnectionParams.setConnectionTimeout(httpParams, 300000);
+            HttpConnectionParams.setSoTimeout(httpParams, 300000);
+            /*SchemeRegistry schemeRegistry = new SchemeRegistry();
+            schemeRegistry
+                    .register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 8085));
+            ClientConnectionManager cm = new ThreadSafeClientConnManager(httpParams, schemeRegistry);*/
+            client = new DefaultHttpClient(httpParams);
+        }
+    }
 
-	public boolean putFile(String file, String uid, String token,
-			InputStream is, long length) {
-		String url = String.format(MYPREFERENCES_URL_TEP, uid, file);
-		String authInfo = String.format("ASP %s:%s", uid, token);
-		HttpPut p = new HttpPut(url);
-		p.setEntity(new InputStreamEntity(is, length));
-		p.setHeader("Authorization", authInfo);
-		try {
-			HttpResponse r = client.execute(p);
-			if (r.getStatusLine().getStatusCode() == HttpURLConnection.HTTP_OK) {
-				return true;
-			}
-		} catch (IOException e) {
-		}
-		return false;
-	}
+    public static CloudStorage getInstance() {
+        if (mInstance == null)
+            mInstance = new CloudStorage();
+        return mInstance;
+    }
 
-	public boolean deleteFile(String file, String uid, String token) {
+    public boolean putFile(String file, String uid, String token, InputStream is, long length) {
+        String url = String.format(MYPREFERENCES_URL_TEP, uid, file);
+        String authInfo = String.format("ASP %s:%s", uid, token);
+        HttpPut p = new HttpPut(url);
+        p.setEntity(new InputStreamEntity(is, length));
+        p.setHeader("Authorization", authInfo);
+        p.setHeader("Content-Range", String.format("bytes %d-%d/%d", 0, length - 1, length));
+        try {
+            HttpResponse r = client.execute(p);
+            if (r.getStatusLine().getStatusCode() == HttpURLConnection.HTTP_OK) {
+                return true;
+            }
+        } catch (IOException e) {
+        }
+        return false;
+    }
 
-		String authInfo = String.format("ASP %s:%s", uid, token);
-		URL url;
-		try {
-			url = new URL(String.format(MYPREFERENCES_URL_TEP, uid, file));
-			URLConnection connection = url.openConnection();
-			HttpURLConnection httpConnection = (HttpURLConnection) connection;
-			httpConnection.setRequestMethod("DELETE");
-			connection.setRequestProperty("Authorization", authInfo);
-			int responseCode = httpConnection.getResponseCode();
-			if (responseCode == HttpURLConnection.HTTP_OK) {
-				return true;
-			}
-		} catch (MalformedURLException e) {
-		} catch (IOException e) {
-		}
+    public String getMd5(File tmpFile) {
 
-		return false;
-	}
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e2) {
+            return null;
+        }
+        InputStream is = null;
+        try {
+            is = new FileInputStream(tmpFile);
+        } catch (FileNotFoundException e2) {
+            return null;
+        }
+        try {
+            byte[] buffer = new byte[8192];
+            int length;
+            while ((length = is.read(buffer)) != -1) {
+                md.update(buffer, 0, length);
+            }
+            is.close();
+        } catch (IOException e) {
+            return null;
+        }
+        byte[] digest = md.digest();
+        return Base64.encodeToString(digest, Base64.DEFAULT);
+    }
 
-	public InputStream downloadFile(String file, String uid, String token) {
-		String url = String.format(MYPREFERENCES_URL_TEP, uid, file);
-		String authInfo = String.format("ASP %s:%s", uid, token);
-		HttpGet g = new HttpGet(url);
-		g.setHeader("Authorization", authInfo);
-		try {
-			HttpResponse r = client.execute(g);
-			return r.getEntity().getContent();
-		} catch (IOException e) {
-		}
+    public boolean putFile(String file, String remoteFile, String uid, String token) {
+        String url = String.format(MYPREFERENCES_URL_TEP, uid, remoteFile);
+        String authInfo = String.format("ASP %s:%s", uid, token);
+        File f = new File(file);
+        String md5 = getMd5(f);
+        Log.d(TAG, "md5 is " + md5);
+        long transferLength = f.length();
+        Log.d(TAG, "file length is " + f.length());
+        long transferedLength = 0;
+        while (transferLength > 0) {
+            FileInputStream is = null;
+            try {
+                is = new FileInputStream(f);
+                is.skip(transferedLength);
+                Log.d(TAG, "open input stream");
+            } catch (IOException e) {
+                return false;
+            }
+            HttpPut p = new HttpPut(url);
+            p.setHeader("Authorization", authInfo);
+            long size = transferLength > BLOCK_SIZE ? BLOCK_SIZE : transferLength;
+            p.setHeader("Content-Range", String.format("bytes %d-%d/%d", transferedLength,
+                    transferedLength + size - 1, f.length()));
+            p.setEntity(new InputStreamEntity(is, size));
+            Log.d(TAG, "before Http post");
+            while (true) {
+                try {
+                    for(Header h : p.getAllHeaders()) {
+                        Log.d(TAG, h.getName() + ":" +h.getValue());
+                    }
+                    HttpResponse r = client.execute(p);
+                    Log.d(TAG, "after Http post " + r.getStatusLine().getStatusCode());
+                    if (r.getStatusLine().getStatusCode() == HttpURLConnection.HTTP_OK) {
+                        transferLength -= size;
+                        transferedLength += size;
+                        is.close();
+                        Log.d(TAG, "Transfered " + transferedLength);
+                        break;
+                    }
+                } catch (ClientProtocolException e) {
+                    Log.d(TAG, e.toString());
+                } catch (IOException e) {
+                    Log.d(TAG, e.toString());
+                }
+            }
+        }
 
-		return null;
-	}
+        return false;
+    }
+
+    public boolean deleteFile(String file, String uid, String token) {
+
+        String authInfo = String.format("ASP %s:%s", uid, token);
+        URL url;
+        try {
+            url = new URL(String.format(MYPREFERENCES_URL_TEP, uid, file));
+            URLConnection connection = url.openConnection();
+            HttpURLConnection httpConnection = (HttpURLConnection) connection;
+            httpConnection.setRequestMethod("DELETE");
+            connection.setRequestProperty("Authorization", authInfo);
+            int responseCode = httpConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                return true;
+            }
+        } catch (MalformedURLException e) {
+        } catch (IOException e) {
+        }
+
+        return false;
+    }
+
+    public InputStream downloadFile(String file, String uid, String token) {
+        String url = String.format(MYPREFERENCES_URL_TEP, uid, file);
+        String authInfo = String.format("ASP %s:%s", uid, token);
+        HttpGet g = new HttpGet(url);
+        g.setHeader("Authorization", authInfo);
+        try {
+            HttpResponse r = client.execute(g);
+            return r.getEntity().getContent();
+        } catch (IOException e) {
+        }
+
+        return null;
+    }
 }
